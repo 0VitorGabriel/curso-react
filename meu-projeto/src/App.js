7c369626b05229596f949f36a68ec500329414e9
0VitorGabriel <victoropo64@outlook.com>
2023-03-31 08:40:31 -0400

    aula finalizada

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
 
 function App() {
+  const name = "Vitor Gabriel"
+
+  const newName = name.toUpperCase()
+
+  let soma = (a, b) => {return a + b}
+
+  const url = "https://via.placeholder.com/150"
+
   return (
     <div className="App">
         <h1>
-          Olá React!
+          Alterando o JSX!
         </h1>
         <p>
-          Meu primeiro app!
+          Olá, {newName}
         </p>
+        <p>
+          soma: {soma(1, 5)}
+        </p>
+        <img src={url} alt="imagem" />
     </div>
   );
 }
